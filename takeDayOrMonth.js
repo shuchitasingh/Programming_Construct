8762a5ee52399cec81debe5584f172168ab7ab53
shuchitasingh <singhshuchita5@gmail.com>
2021-05-28 15:42:12 +0530

    Added:if else problem

---
+++ takeDayOrMonth.js
@@ -0,0 +1,12 @@
+{
+    let month = 3
+    let day = 25
+    if(day<=31&&month<=12){
+        if(day>20&&month>=3&&month<=6)
+            console.log("True");
+        else 
+            console.log("False");
+    }
+    else 
+        console.log("Invalid Date")
+}
