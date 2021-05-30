7b28b29e8a6ecf383ac2957da372b0552839a8c9
shuchitasingh <singhshuchita5@gmail.com>
2021-05-30 18:57:03 +0530

    Added:map method problems

---
+++ mapMethod/problem2.js
@@ -0,0 +1,15 @@
+/*
+   Write a Program to generate a birth month of 50 individuals
+   between the year 92 & 93. Find all the individuals having birthdays
+   in the same month. Store it to finally print.
+*/
+
+let map = new Map([[1, 0], [2, 0], [3, 0], [4, 0], [5, 0], [6, 0], [7, 0], [8, 0], [9, 0], [10, 0], [11, 0], [12, 0]]);
+// gets count of individual in each month
+for (let i = 1; i <= 50; i++) {
+    let month = (Math.floor(Math.random() * 100) % 12) + 1;
+    let valueCount = map.get(month);
+    map.set(month, ++valueCount);
+}
+console.log("month and count of individuals birthday: ")
+console.log(map);
